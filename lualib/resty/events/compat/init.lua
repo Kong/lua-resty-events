ccc36c930f9b56ea6c81fe047902396d079d25b5
chronolaw <chrono_cpp@me.com>
2022-04-09 17:26:27 +0800

    clean compat

--- lualib/resty/events/compat/init.lua
+++ lualib/resty/events/compat/init.lua
@@ -1,6 +1,6 @@
 -- compatible with lua-resty-worker-events
 
-local events = require("resty.events").new()
+local ev = require("resty.events").new()
 
 local _M = {
     _VERSION = '0.1.0',
@@ -11,23 +11,23 @@ function _M.poll()
 end
 
 function _M.configure(opts)
-    return events:configure(opts)
+    return ev:configure(opts)
 end
 
 function _M.run()
-    return events:run()
+    return ev:run()
 end
 
 _M.post = function(source, event, data, unique)
-    return events:publish(unique or "all", source, event, data)
+    return ev:publish(unique or "all", source, event, data)
 end
 
 _M.post_local = function(source, event, data)
-    return events:publish("current", source, event, data)
+    return ev:publish("current", source, event, data)
 end
 
 _M.register = function(callback, source, event, ...)
-    return events:subscribe(source or "*", event or "*", callback)
+    return ev:subscribe(source or "*", event or "*", callback)
 end
 
 _M.register_weak = _M.register
