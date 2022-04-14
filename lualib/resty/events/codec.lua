9a22c94da161187c645ece8c8343016dce8a58e1
chronolaw <chrono_cpp@me.com>
2022-04-14 08:35:26 +0800

    code clean

--- lualib/resty/events/codec.lua
+++ lualib/resty/events/codec.lua
@@ -1,8 +1,8 @@
-local cjson = require "cjson.safe"
+-- string.buffer is introduced since openresty 1.21.4.1
 
 local ok, buffer = pcall(require, "string.buffer")
 if not ok then
-    return cjson
+    return require "cjson.safe"
 
 else
     return buffer
