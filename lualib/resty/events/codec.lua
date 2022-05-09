02c678093a3d32fa3a8ee62a3df4f31d293a0690
chronolaw <chrono_cpp@me.com>
2022-05-09 20:05:39 +0800

    codec.lua

--- lualib/resty/events/codec.lua
+++ lualib/resty/events/codec.lua
@@ -5,17 +5,19 @@ if not ok then
     return require "cjson.safe"
 end
 
+
 local options = {
   dict = { "spec", "data",
            "source", "event", "wid", "unique",
          },
 }
 
+
 local buf_enc = buffer.new(options)
 local buf_dec = buffer.new(options)
 
+
 local _M = {}
-local _MT = { __index = _M, }
 
 
 function _M.encode(obj)
