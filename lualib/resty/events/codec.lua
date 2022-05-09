bd1abe06c0e19234be7b1aa6bea0d2e978af5a05
chronolaw <chrono_cpp@me.com>
2022-05-09 15:27:31 +0800

    optimize for string.buffer

--- lualib/resty/events/codec.lua
+++ lualib/resty/events/codec.lua
@@ -3,8 +3,27 @@
 local ok, buffer = pcall(require, "string.buffer")
 if not ok then
     return require "cjson.safe"
-
-else
-    return buffer
 end
+
+local options = {
+  dict = { "spec", "data",
+           "source", "event", "wid", "unique",
+         },
+}
+
+local buf_enc = buffer.new(options)
+local buf_dec = buffer.new(options)
+
+local function encode(obj)
+  return buf_enc:reset():encode(obj):get()
+end
+
+local function decode(str)
+  return buf_dec:set(str):decode()
+end
+
+return {
+    encode = encode,
+    decode = decode,
+}
 
