Kong/lua-resty-events

commit 0f820d52702f321c44a03b089456c372e53882d9

fix(worker): do not premature kill threads when not exiting (#47)

lualib/resty/events/worker.lua
@@ -1,12 +1,11 @@
 local cjson = require "cjson.safe"
 local codec = require "resty.events.codec"
-local que = require "resty.events.queue"
+local queue = require "resty.events.queue"
 local callback = require "resty.events.callback"
 
+local frame_validate = require("resty.events.frame").validate
 local client = require("resty.events.protocol").client
 local is_timeout = client.is_timeout
-
-local frame_validate = require("resty.events.frame").validate
 
 local type = type
 local assert = assert
@@ -65,14 +64,20 @@ local function random_delay()
     return random(10, 50) / 1000
 end
 
-local function do_event(self, d)
-    self._callback:do_event(d)
+local function communicate(premature, self)
+    if premature then
+        return
+    end
+
+    self:communicate()
 end
 
 local function start_timer(self, delay)
-    assert(timer_at(delay, function(premature)
-        self:communicate(premature)
-    end))
+    assert(timer_at(delay, communicate, self))
+end
+
+local function terminating(self)
+    return not self._connected or exiting()
 end
 
 local check_sock_exist
@@ -80,7 +85,7 @@ do
     local ffi = require "ffi"
     local C = ffi.C
     ffi.cdef [[
-    int access(const char *pathname, int mode);
+        int access(const char *pathname, int mode);
     ]]
 
     -- remove prefix 'unix:'
@@ -94,8 +99,8 @@ function _M.new(opts)
     local max_queue_len = opts.max_queue_len
 
     local self = {
-        _pub_queue = que.new(max_queue_len),
-        _sub_queue = que.new(max_queue_len),
+        _pub_queue = queue.new(max_queue_len),
+        _sub_queue = queue.new(max_queue_len),
         _callback = callback.new(),
         _connected = nil,
         _opts = opts,
@@ -104,12 +109,106 @@ function _M.new(opts)
     return setmetatable(self, _MT)
 end
 
-function _M:communicate(premature)
-    if premature then
-        -- worker wants to exit
-        return
-    end
+local function read_thread(self, broker_connection)
+    while not terminating(self) do
+        local data, err = broker_connection:recv_frame()
+        if err then
+            if not is_timeout(err) then
+                return nil, "failed to read event: " .. err
+            end
 
+            -- timeout
+            goto continue
+        end
+
+        if not data then
+            if not exiting() then
+                log(ERR, "did not receive event from broker")
+            end
+            goto continue
+        end
+
+        local event_data, err = decode(data)
+        if err then
+            if not exiting() then
+                log(ERR, "failed to decode event data: ", err)
+            end
+            goto continue
+        end
+
+        -- got an event data, push to queue, callback in events_thread
+        local _, err = self._sub_queue:push(event_data)
+        if err then
+            if not exiting() then
+                log(ERR, "failed to store event: ", err, ". data is: ",
+                         cjson_encode(event_data))
+            end
+            goto continue
+        end
+
+        ::continue::
+    end -- while not terminating
+
+    return true
+end
+
+local function write_thread(self, broker_connection)
+    local counter = 0
+
+    while not terminating(self) do
+        local payload, err = self._pub_queue:pop()
+        if err then
+            if not is_timeout(err) then
+                return nil, "semaphore wait error: " .. err
+            end
+
+            -- timeout
+            goto continue
+        end
+
+        local _, err = broker_connection:send_frame(payload)
+        if err then
+            return nil, "failed to send event: " .. err
+        end
+
+        -- events rate limiting
+        counter = counter + 1
+        if counter >= EVENTS_COUNT_LIMIT then
+            sleep(EVENTS_SLEEP_TIME)
+            counter = 0
+        end
+
+        ::continue::
+    end -- while not terminating
+
+    return true
+end
+
+local function events_thread(self)
+    while not terminating(self) do
+        local data, err = self._sub_queue:pop()
+        if err then
+            if not is_timeout(err) then
+                return nil, "semaphore wait error: " .. err
+            end
+
+            -- timeout
+            goto continue
+        end
+
+        -- got an event data, callback
+        self._callback:do_event(data)
+
+        -- yield, not block other threads
+        sleep(0)
+
+        ::continue::
+    end -- while not terminating
+
+    return true
+end
+
+function _M:communicate()
     -- only for testing, skip read/write/events threads
     if self._opts.testing == true then
         self._connected = true
@@ -126,9 +225,9 @@ function _M:communicate(premature)
         return
     end
 
-    local conn = assert(client:new())
+    local broker_connection = assert(client.new())
 
-    local ok, err = conn:connect(listening)
+    local ok, err = broker_connection:connect(listening)
 
     if exiting() then
         return
@@ -143,118 +242,22 @@ function _M:communicate(premature)
         return
     end
 
-    self._connected = true
     log(DEBUG, _worker_id, " on (", listening, ") is ready")
 
-    local read_thread = spawn(function()
-        while not exiting() do
-            local data, err = conn:recv_frame()
+    self._connected = true
 
-            if exiting() then
-                return
-            end
+    local read_thread_co = spawn(read_thread, self, broker_connection)
+    local write_thread_co = spawn(write_thread, self, broker_connection)
+    local events_thread_co = spawn(events_thread, self)
 
-            if err then
-                if not is_timeout(err) then
-                    return nil, err
-                end
-
-                -- timeout
-                goto continue
-            end
-
-            if not data then
-                return nil, "did not receive event from broker"
-            end
-
-            local d, err = decode(data)
-            if not d then
-                return nil, "failed to decode event data: " .. err
-            end
-
-            -- got an event data, push to queue, callback in events_thread
-            local ok, err = self._sub_queue:push(d)
-            if not ok then
-                log(ERR, "failed to store event: ", err, ". ",
-                         "data is :", cjson_encode(d))
-            end
-
-            ::continue::
-        end -- while not exiting
-    end)  -- read_thread
-
-    local write_thread = spawn(function()
-        local counter = 0
-
-        while not exiting() do
-            local payload, err = self._pub_queue:pop()
-
-            if not payload then
-                if not is_timeout(err) then
-                    return nil, "semaphore wait error: " .. err
-                end
-
-                -- timeout
-                goto continue
-            end
-
-            if exiting() then
-                return
-            end
-
-            local _, err = conn:send_frame(payload)
-            if err then
-                log(ERR, "failed to send event: ", err)
-                return
-            end
-
-            -- events rate limiting
-            counter = counter + 1
-            if counter >= EVENTS_COUNT_LIMIT then
-                sleep(EVENTS_SLEEP_TIME)
-                counter = 0
-            end
-
-            ::continue::
-        end -- while not exiting
-    end)  -- write_thread
-
-    local events_thread = spawn(function()
-        while not exiting() do
-            local data, err = self._sub_queue:pop()
-
-            if not data then
-                if not is_timeout(err) then
-                    return nil, "semaphore wait error: " .. err
-                end
-
-                -- timeout
-                goto continue
-            end
-
-            if exiting() then
-                return
-            end
-
-            -- got an event data, callback
-            do_event(self, data)
-
-            -- yield, not block other threads
-            sleep(0)
-
-            ::continue::
-        end -- while not exiting
-    end)  -- events_thread
-
-    local ok, err, perr = wait(write_thread, read_thread, events_thread)
-
-    kill(write_thread)
-    kill(read_thread)
-    kill(events_thread)
+    local ok, err, perr = wait(read_thread_co, write_thread_co, events_thread_co)
 
     self._connected = nil
 
     if exiting() then
+        kill(read_thread_co)
+        kill(write_thread_co)
+        kill(events_thread_co)
         return
     end
 
@@ -265,6 +268,10 @@ function _M:communicate(premature)
     if perr then
         log(ERR, "event worker failed: ", perr)
     end
+
+    wait(read_thread_co)
+    wait(write_thread_co)
+    wait(events_thread_co)
 
     start_timer(self, random_delay())
 end
@@ -335,7 +342,7 @@ function _M:publish(target, source, event, data)
     if self._opts.testing == true then
         log(DEBUG, "event published to 1 workers")
 
-        do_event(self, {
+        self._callback:do_event({
             source = source,
             event = event,
             data = data,
@@ -368,7 +375,7 @@ end
 function _M:subscribe(source, event, callback)
     assert(type(source) == "string" and source ~= "", "source is required")
     assert(type(event) == "string" and event ~= "", "event is required")
-    assert(type(callback) == "function", "expected function, got: "..
+    assert(type(callback) == "function", "expected function, got: " ..
            type(callback))
 
     return self._callback:subscribe(source, event, callback)

t/deadlock.t
@@ -1,0 +1,156 @@
+# vim:set ft= ts=4 sw=4 et fdm=marker:
+use Test::Nginx::Socket::Lua;
+
+plan tests => repeat_each() * (blocks() * 11);
+
+$ENV{TEST_NGINX_HTML_DIR} ||= html_dir();
+
+master_on();
+workers(3);
+run_tests();
+
+__DATA__
+
+=== TEST 1: posting events and handling events, broadcast
+--- http_config
+    lua_package_path "../lua-resty-core/lib/?.lua;lualib/?/init.lua;lualib/?.lua;;";
+    lua_shared_dict dict 1m;
+    init_by_lua_block {
+        require("ngx.process").enable_privileged_agent(4096)
+    }
+    init_worker_by_lua_block {
+        local opts = {
+            broker_id = 2,
+            listening = "unix:$TEST_NGINX_HTML_DIR/nginx.sock",
+        }
+
+        local ev = require("resty.events").new(opts)
+        if not ev then
+            ngx.log(ngx.ERR, "failed to new events")
+        end
+
+        local ok, err = ev:init_worker()
+        if not ok then
+            ngx.log(ngx.ERR, "failed to init_worker events: ", err)
+        end
+
+        local semaphore = require("ngx.semaphore")
+        local sema = semaphore.new(1)
+
+        local id = ngx.worker.id()
+
+        if id == 2 then
+            ngx.shared.dict:set("broker-pid", ngx.worker.pid())
+        end
+
+        if id == 0 or id == 1 then
+            ev:subscribe("content_by_lua", "first", function()
+                ngx.sleep(0)
+                ngx.log(ngx.INFO, id, " first handler")
+                local ok, err = sema:wait(0)
+                if ok then
+                    ngx.log(ngx.INFO, id, " first got mutex")
+                    ngx.sleep(5)
+                else
+                    ngx.log(ngx.CRIT, id, " first did not got mutex: ", err)
+                end
+                sema:post()
+            end)
+        end
+
+        if id == nil then
+            ev:subscribe("content_by_lua", "second", function()
+                ngx.sleep(0)
+                ngx.log(ngx.INFO, "privileged killer")
+                ngx.timer.at(0, function()
+                    ngx.log(ngx.INFO, "killing ", ngx.shared.dict:get("broker-pid"))
+                    os.execute("kill -9 " .. ngx.shared.dict:get("broker-pid"))
+                    ngx.sleep(0)
+                end)
+            end)
+        end
+
+        if id == 0 or id == 1 then
+            ev:subscribe("content_by_lua", "third", function()
+                ngx.sleep(0)
+                ngx.log(ngx.INFO, id, " third handler")
+                local ok, err = sema:wait(0)
+                if ok then
+                    ngx.log(ngx.INFO, id, " third got mutex")
+                else
+                    ngx.log(ngx.CRIT, id, " third did not got mutex: ", err)
+                end
+                sema:post()
+            end)
+        end
+
+
+        -- These tests are left to be enabled when we get this reliable (needs more reliability fixes in lib):
+        --if id == 0 or id == 1 then
+        --    ev:subscribe("content_by_lua", "fourth", function()
+        --        ngx.sleep(0)
+        --        ngx.log(ngx.INFO, id, " fourth handler")
+        --    end)
+        --end
+
+        --ev:subscribe("content_by_lua", "fifth", function()
+        --    ngx.sleep(0)
+        --    ngx.log(ngx.INFO, id or "privileged", " fifth handler")
+        --end)
+
+        _G.ev = ev
+    }
+
+    server {
+        listen unix:$TEST_NGINX_HTML_DIR/nginx.sock;
+        location / {
+            content_by_lua_block {
+                 _G.ev:run()
+            }
+        }
+    }
+--- config
+    location = /test {
+        access_by_lua_block {
+            local ev = _G.ev
+            ev:publish("all", "content_by_lua", "first")
+            ngx.sleep(0)
+            ev:publish("all", "content_by_lua", "second")
+            ngx.sleep(0)
+            ev:publish("all", "content_by_lua", "third")
+            ngx.sleep(0)
+            -- These tests are left to be enabled when we get this reliable (needs more reliability fixes in lib):
+            --ev:publish("all", "content_by_lua", "fourth")
+            --ngx.sleep(0)
+            --ev:publish("all", "content_by_lua", "fifth")
+            --ngx.sleep(0)
+            ngx.say("ok")
+        }
+    }
+--- request
+GET /test
+--- wait: 10
+--- response_body
+ok
+--- no_error_log
+[crit]
+--- error_log eval
+[
+    qr/0 first handler/,
+    qr/1 first handler/,
+    qr/0 first got mutex/,
+    qr/1 first got mutex/,
+    qr/privileged killer/,
+    qr/exited on signal 9/,
+    qr/0 third got mutex/,
+    qr/1 third got mutex/,
+    # These tests are left to be enabled when we get this reliable (needs more reliability fixes in lib):
+    # qr/0 fourth handler/,
+    # qr/1 fourth handler/,
+    # qr/privileged fifth handler/,
+    # qr/0 fifth handler/,
+    # qr/1 fifth handler/,
+    # qr/2 fifth handler/,
+]
+--- skip_nginx
+11: < 1.21.4

commit 929e22104dcf7125ffc75b1a6e53f24ee6cb8df7

chore(*): release 0.2.1
### Summary

- fix(tests): fix tests to not be order dependent on multi-worker scenario (#46)
- chore(callback): remove unnecessary assert when the type is already checked (#50)
- chore(queue): rename DEFAULT_QUEUE_LEN to more meaningful MAX_QUEUE_PREALLOCATE (#52)
- chore(callback): split get_callback_list to two functions to avoid unnecessary table creation (#51)
- fix(worker): do not premature kill threads when not exiting (#47)
- fix(broker): do not premature kill threads when not exiting (#48)
- fix(worker): separate communication and event processing to different timers (#53) - caused by #47

.github/workflows/tests.yml
@@ -13,7 +13,7 @@ jobs:
         # TODO: arm64
         - openresty: "1.19.9.1"
         - openresty: "1.21.4.1"
-        - openresty: "1.21.4.2"
+        - openresty: "1.21.4.3"
         - openresty: "1.25.3.1"
 
     env:
@@ -62,4 +62,3 @@ jobs:
         export PATH=${BASE_PATH}/openresty/bin:$PATH
         openresty -V
         make test OPENRESTY_PREFIX=${BASE_PATH}/openresty
-

LICENSE
@@ -186,7 +186,7 @@
       same "printed page" as the copyright notice for easier
       identification within third-party archives.
 
-   Copyright [yyyy] [name of copyright owner]
+   Copyright 2022-2024 Kong Inc.
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.

lualib/resty/events/broker.lua
@@ -140,10 +140,7 @@ local function write_thread(self, worker_connection)
     return true
 end
 
-local _M = {
-    _VERSION = '0.1.3',
-}
-
+local _M = {}
 local _MT = { __index = _M, }
 
 function _M.new(opts)

lualib/resty/events/callback.lua
@@ -14,9 +14,7 @@ local DEBUG = ngx.DEBUG
 
 local encode = cjson.encode
 
-local _M = {
-  _VERSION = '0.1.0',
-}
+local _M = {}
 local _MT = { __index = _M, }
 
 function _M.new()

lualib/resty/events/codec.lua
@@ -17,9 +17,7 @@ local buf_enc = buffer.new(options)
 local buf_dec = buffer.new(options)
 
 
-local _M = {
-    _VERSION = "0.1.0",
-}
+local _M = {}
 
 
 function _M.encode(obj)

lualib/resty/events/compat/init.lua
@@ -14,7 +14,7 @@ local handlers = {}
 local _configured
 
 local _M = {
-    _VERSION = '0.1.0',
+    _VERSION = "0.1.0",
 }
 
 function _M.poll()
@@ -29,7 +29,6 @@ function _M.configure(opts)
     ev = require("resty.events").new(opts)
 
     local ok, err = ev:init_worker()
-
     if not ok then
         return nil, err
     end
@@ -49,7 +48,6 @@ end
 
 _M.post = function(source, event, data, unique)
     local ok, err = ev:publish(unique or "all", source, event, data)
-
     if not ok then
         return nil, err
     end
@@ -59,7 +57,6 @@ end
 
 _M.post_local = function(source, event, data)
     local ok, err = ev:publish("current", source, event, data)
-
     if not ok then
         return nil, err
     end
@@ -69,19 +66,16 @@ end
 
 _M.register = function(callback, source, event, ...)
     local events = {event or "*", ...}
-
     for _, e in ipairs(events) do
         local id = ev:subscribe(source or "*", e or "*", callback)
-
         handlers[callback] = id
     end
 end
 
 _M.register_weak = _M.register
 
-_M.unregister = function(callback, source, ...)
+_M.unregister = function(callback)
     local id = handlers[callback]
-
     if not id then
         return
     end

lualib/resty/events/frame.lua
@@ -14,9 +14,7 @@ local assert = assert
 local tostring = tostring
 
 
-local _M = {
-    _VERSION = "0.2.0",
-}
+local _M = {}
 
 
 -- frame format: Len(3 bytes) + Payload(max to 2^24 - 1 bytes)

lualib/resty/events/init.lua
@@ -13,7 +13,7 @@ local str_sub = string.sub
 local worker_count = ngx.worker.count()
 
 local _M = {
-    _VERSION = '0.2.0',
+    _VERSION = "0.2.1",
 }
 local _MT = { __index = _M, }
 

lualib/resty/events/protocol.lua
@@ -47,7 +47,6 @@ local function send_frame(self, payload)
 end
 
 local _Server = {
-    _VERSION = "0.1.0",
     is_closed = is_closed,
     is_timeout = is_timeout,
     recv_frame = recv_frame,
@@ -91,7 +90,6 @@ end
 
 
 local _Client = {
-    _VERSION = "0.1.0",
     is_closed = is_closed,
     is_timeout = is_timeout,
     recv_frame = recv_frame,

lualib/resty/events/worker.lua
@@ -53,9 +53,7 @@ local PAYLOAD_T = {
 --local _worker_pid = ngx.worker.pid()
 local _worker_id = ngx.worker.id() or -1
 
-local _M = {
-    _VERSION = '0.2.0',
-}
+local _M = {}
 local _MT = { __index = _M, }
 
 -- gen a random number [0.01, 0.05]

src/ngx_lua_events_module.c
@@ -1,5 +1,5 @@
 /**
- * Copyright 2019-2022 Kong Inc.
+ * Copyright 2019-2024 Kong Inc.
 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -89,4 +89,3 @@ ngx_lua_ffi_disable_listening_unix_socket(ngx_str_t *sock_name)
 
 #endif
 }
-